e19166396560aae7186a60845aa63984b3f47863
AndrewHack242 <andrew.hack242@gmail.com>
2024-07-20 22:57:37 -0600

    Add project files.

---
+++ Lethal_Weight_Fix/Patches/Lethal_weight_fix.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using GameNetcodeStuff;
+using HarmonyLib;
+
+
+namespace Lethal_Weight_Fix.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class Lethal_weight_fix
+    {
+        // make one function to deliver the patch the same way to every required location.
+        static IEnumerable<CodeInstruction> deliver_patch(IEnumerable<CodeInstruction> instructions, string func_name)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            for (int i = 0; i < codes.Count; ++i)
+            {
+                if (codes[i].Calls(typeof(UnityEngine.Mathf).GetMethod("Clamp", new Type[] { typeof(float), typeof(float), typeof(float) })))
+                {
+                    Lethal_weight_fix_base.LogInfo("Found " + codes[i] + "in function " + func_name);
+                    if (i < 3)
+                    {
+                        Lethal_weight_fix_base.LogInfo("index was too small on match in " + codes[i] + "in function " + func_name + ", index: " + i);
+                        continue;
+                    }
+                    codes.Insert(i + 1, codes[i - 3]);
+                    Lethal_weight_fix_base.LogInfo("opcode = " + codes[i - 2].opcode);
+                    Lethal_weight_fix_base.LogInfo("operand = " + codes[i - 2].operand);
+                    codes[i - 2].operand = 0f;
+                    Lethal_weight_fix_base.LogInfo("updated opcode = " + codes[i - 2].opcode);
+                    Lethal_weight_fix_base.LogInfo("updated operand = " + codes[i - 2].operand);
+                    codes.RemoveAt(i - 3);
+                    Lethal_weight_fix_base.LogInfo("Patched function " + func_name);
+                }
+            }
+            return codes;
+        }
+
+        [HarmonyPatch("BeginGrabObject")]
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> patch_BeginGrabObject(IEnumerable<CodeInstruction> instructions)
+        {
+            return deliver_patch(instructions, "BeginGrabObject");
+        }
+
+        // maybe not needed? - was like this before? but it probably shouldn't be
+        [HarmonyPatch("GrabObject")]
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> patch_GrabObject(IEnumerable<CodeInstruction> instructions)
+        {
+            return deliver_patch(instructions, "GrabObject");
+        }
+
+        [HarmonyPatch("GrabObjectClientRpc")]
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> patch_GrabObjectClientRpc(IEnumerable<CodeInstruction> instructions)
+        {
+            return deliver_patch(instructions, "GrabObjectClientRpc");
+        }
+
+        [HarmonyPatch("DespawnHeldObjectOnClient")]
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> patch_DespawnHeldObjectOnClient(IEnumerable<CodeInstruction> instructions)
+        {
+            return deliver_patch(instructions, "DespawnHeldObjectOnClient");
+        }
+
+        [HarmonyPatch("SetObjectAsNoLongerHeld")]
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> patch_SetObjectAsNoLongerHeld(IEnumerable<CodeInstruction> instructions)
+        {
+            return deliver_patch(instructions, "SetObjectAsNoLongerHeld");
+        }
+
+        [HarmonyPatch("PlaceGrabbableObject")]
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> patch_PlaceGrabbableObject(IEnumerable<CodeInstruction> instructions)
+        {
+            return deliver_patch(instructions, "PlaceGrabbableObject");
+        }
+
+        [HarmonyPatch("DestroyItemInSlot")]
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> patch_DestroyItemInSlot(IEnumerable<CodeInstruction> instructions)
+        {
+            return deliver_patch(instructions, "DestroyItemInSlot");
+        }
+
+        [HarmonyPatch("BeginGrabObject")]
+        [HarmonyPostfix]
+        static void patch_BeginGrabObject_postfix(ref GrabbableObject ___currentlyGrabbingObject)
+        {
+            Lethal_weight_fix_base.LogDebug("object weight = " + ___currentlyGrabbingObject.itemProperties.weight);
+        }
+
+    }
+}
---
+++ Lethal_Weight_Fix/Plugin.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace Lethal_Weight_Fix
+{
+    [BepInPlugin(mod_GUID, mod_name, mod_version)]
+    public class Lethal_weight_fix_base : BaseUnityPlugin
+    {
+        private const string mod_GUID = "Hackattack242.Lethal_Weight_Fix";
+        private const string mod_name = "Lethal_Weight_Fix";
+        private const string mod_version = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(mod_GUID);
+
+        private static Lethal_weight_fix_base Instance;
+
+        private ManualLogSource logger;
+
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            logger = BepInEx.Logging.Logger.CreateLogSource(mod_name);
+
+            logger.LogInfo("Lethal Weight Fix has awoken");
+
+            harmony.PatchAll();
+
+            logger.LogInfo("Lethal Weight Fix Patches Applied");
+        }
+
+        internal static void LogDebug(string message)
+        {
+            Instance.Log(message, (LogLevel)32);
+        }
+
+        internal static void LogInfo(string message)
+        {
+            Instance.Log(message, (LogLevel)16);
+        }
+
+        internal static void LogWarning(string message)
+        {
+            Instance.Log(message, (LogLevel)4);
+        }
+
+        internal static void LogError(string message)
+        {
+            Instance.Log(message, (LogLevel)2);
+        }
+
+        internal static void LogError(Exception ex)
+        {
+            Instance.Log(ex.Message + "\n" + ex.StackTrace, (LogLevel)2);
+        }
+
+        private void Log(string message, LogLevel logLevel)
+        {
+            ((Lethal_weight_fix_base)this).logger.Log(logLevel, (object)message);
+        }
+
+    }
+
+}
